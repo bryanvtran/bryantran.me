13260ed43ca10396005d52613d7b09d7c1b9937c
Bryan Tran <bryanvantran@ucla.edu>
2016-10-02 18:49:52 -0700

    adding project data and small fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask, render_template, json, jsonify
+from collections import OrderedDict
 app = Flask(__name__)
 
 def getJson(name):
         SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
         json_url = os.path.join(SITE_ROOT, 'static', 'data/' + name)
-        projects = json.load(open(json_url))
+        projects = json.load(open(json_url), object_pairs_hook=OrderedDict)
         return projects
 
 # Homepage
@@ -21,7 +22,7 @@ def stock(project):
     for p in projects:
         if p == project:
             selected_project = projects[p]
-    return render_template('item.html', project=selected_project)
+    return render_template('item.html', page=project, project=selected_project)
 
 if __name__ == "__main__":
     app.run(debug=True)
